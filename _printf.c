7f99ad8cfbe99a55f9beafe1452ba4997db86480
abdoneverland <ch.abdel.news@gmail.com>
2023-04-22 16:01:57 -0700

    task 0 & 1

--- _printf.c
+++ _printf.c
@@ -12,7 +12,7 @@ int _printf(const char *const format, ...)
 	va_start(ap, format);
 	for (i = 0; format && format[i] != '\0'; i++)
 	{
-		if (format[i] == '%')
+		if (format[i] == '%' && format[i + 1] != '\0')
 		{
 			i++;
 			switch (format[i])
