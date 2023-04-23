31c2e25ace494d76238bba530ec2b72e18e3d83c
abdoneverland <ch.abdel.news@gmail.com>
2023-04-22 18:23:04 -0700

    task 0 & 1

--- _printf.c
+++ _printf.c
@@ -1,5 +1,38 @@
 #include "main.h"
 /**
+ * handle_conversion - handle conversion specifiers
+ *@format: format
+ *@i: iterator position
+ *@ap: va_list
+ *@nb_printed: number of printed char
+ */
+void handle_conversion(const char *const format,
+		int i, va_list ap, int *nb_printed)
+{
+	switch (format[i])
+	{
+		case 'd':
+		case 'i':
+			print_int(va_arg(ap, int), nb_printed);
+			break;
+		case 'b':
+			/*to complete*/
+			break;
+		case 'c':
+			_putchar((va_arg(ap, int)));
+			(*nb_printed)++;
+			break;
+		case 's':
+			print_string(va_arg(ap, char *), nb_printed);
+			break;
+		default:
+			_putchar('%');
+			if (format[i] != '%')
+				_putchar(format[i]), (*nb_printed)++;
+			(*nb_printed)++;
+	}
+}
+/**
  * _printf - alternative to printf
  *@format: format
  * Return: int
@@ -15,30 +48,12 @@ int _printf(const char *const format, ...)
 		if (format[i] == '%')
 		{
 			if (format[i + 1] == '\0')
-				nb_printed = -1, break;
-			i++;
-			switch (format[i])
 			{
-				case 'd':
-				case 'i':
-					print_int(va_arg(ap, int), &nb_printed);
-					break;
-				case 'b':
-					/*to complete*/
-					break;
-				case 'c':
-					_putchar((va_arg(ap, int)));
-					nb_printed++;
-					break;
-				case 's':
-					print_string(va_arg(ap, char *), &nb_printed);
-					break;
-				default:
-					_putchar('%');
-					if (format[i] != '%')
-						_putchar(format[i]), nb_printed++;
-					nb_printed++;
+				nb_printed = -1;
+				break;
 			}
+			i++;
+			handle_conversion(format, i, ap, &nb_printed);
 		}
 		else
 			_putchar(format[i]), nb_printed++;
