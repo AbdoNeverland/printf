f6286902be490f9fe21c7732c0c6e0f04a921fe0
abdoneverland <ch.abdel.news@gmail.com>
2023-04-23 13:11:53 -0700

    task 2

--- main.h
+++ main.h
@@ -8,4 +8,5 @@ void _putchar(char c);
 void print_string(char *str, int *nb_printed);
 int _printf(const char *const format, ...);
 void print_int(long int n, int *nb_printed);
+void print_b(long int n, int *nb_printed);
 #endif
---
+++ print_b.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_b - convert and print a number to binary
+ *@n: number to convert
+ *@nb_printed: number of characters printed
+ * Return: void
+ */
+void print_b(long int n, int *nb_printed)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		(*nb_printed)++;
+		n *= -1;
+	}
+	if (n / 2 > 0)
+		print_b(n / 2, nb_printed);
+	_putchar('0' + n % 2);
+	(*nb_printed)++;
+}
