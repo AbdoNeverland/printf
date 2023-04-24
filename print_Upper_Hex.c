d8ce616a4504d30e5cd6f2b4bb571e454e6dbc8f
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-24 08:49:48 -0700

    task 3

--- print_Upper_Hex.c
+++ print_Upper_Hex.c
@@ -7,26 +7,26 @@
  */
 void print_Upper_Hexa(unsigned int n, int *nb_printed)
 {
-char hex_digits[] = "0123456789ABCDEF";
-char hex_num[32];
-int num_digits = 0, i;
+	char hex_digits[] = "0123456789ABCDEF";
+	char hex_num[32];
+	int num_digits = 0, i;
 
-if (n == 0)
-{
-putchar('0');
-(*nb_printed)++;
-return;
-}
+	if (n == 0)
+	{
+		putchar('0');
+		(*nb_printed)++;
+		return;
+	}
 
-while (n != 0)
-{
-hex_num[num_digits++] = hex_digits[n % 16];
-n /= 16;
-}
+	while (n != 0)
+	{
+		hex_num[num_digits++] = hex_digits[n % 16];
+		n /= 16;
+	}
 
-for (i = num_digits - 1; i >= 0; i--)
-{
-putchar(hex_num[i]);
-(*nb_printed)++;
-}
+	for (i = num_digits - 1; i >= 0; i--)
+	{
+		putchar(hex_num[i]);
+		(*nb_printed)++;
+	}
 }
