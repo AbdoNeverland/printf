9eeb64b8ef39a7fb9b5bb177655cb49734f06b2a
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-24 07:37:02 -0700

    task 3

--- print_unsignedInt.c
+++ print_unsignedInt.c
@@ -7,18 +7,10 @@
  */
 void print_unsignedInt(unsigned int n, int *nb_printed)
 {
-	unsigned int a = 4294967295;
-
-	if (!n)
-	{
-		a +=  (n + 1);
-		print_unsignedInt(a, nb_printed);
-	}
-	if (n / 10)
+	if (n >= 10)
 	{
 		print_unsignedInt(n / 10, nb_printed);
 	}
-	a = (n % 10) + '0';
-	_putchar(a);
+	putchar(n % 10 + '0');
 	(*nb_printed)++;
 }
