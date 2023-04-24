e6665e0537aeca162c676f37bf6484ace5e0993a
Achraf ibn cheikh <bnachraf4@gmail.com>
2023-04-24 08:20:28 -0700

    task 3

--- print_Lower_Hexa.c
+++ print_Lower_Hexa.c
@@ -7,21 +7,26 @@
  */
 void print_Lower_Hexa(unsigned int n, int *nb_printed)
 {
-	int a = n % 16;
+char hex_digits[] = "0123456789abcdef";
+char hex_num[32];
+int num_digits = 0, i;
 
-	if (n > 0)
-	{
-		if (a < 10)
-		{
-			print_Lower_Hexa(n / 16, nb_printed);
-			_putchar(a + '0');
-			(*nb_printed)++;
-		}
-		else
-		{
-			print_Lower_Hexa(n / 16, nb_printed);
-			_putchar(a + '7' - 'A' + 'a');
-			(*nb_printed)++;
-		}
-	}
+if (n == 0)
+{
+putchar('0');
+(*nb_printed)++;
+return;
+}
+
+while (n != 0)
+{
+hex_num[num_digits++] = hex_digits[n % 16];
+n /= 16;
+}
+
+for (i = num_digits - 1; i >= 0; i--)
+{
+putchar(hex_num[i]);
+(*nb_printed)++;
+}
 }
