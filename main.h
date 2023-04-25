907196bb2dda7884e7af70e8d792ea347a04eacb
abdoneverland <ch.abdel.news@gmail.com>
2023-04-25 23:40:47 +0100

    replacing switch

--- main.h
+++ main.h
@@ -4,27 +4,33 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
- * struct spe_fun - struct
+ * struct spec_fun - struct
  *@specifier: the specifier
  *@function: function pointer
  */
 struct spec_fun
 {
-    char specifier;
-    void (*function)(const char *const format, int i, va_list ap, int *nb_printed);
+	char specifier;
+	void (*function)(const char *const format,
+			int i, va_list ap, int *nb_printed);
 };
 typedef struct spec_fun spe_fun;
 void _putchar(char c);
 void print_c(const char *const format, int i, va_list ap, int *nb_printed);
-void print_string(const char *const format, int i, va_list ap, int *nb_printed);
-void print_rstring(const char *const format, int i, va_list ap, int *nb_printed);
+void print_string(const char *const format,
+		int i, va_list ap, int *nb_printed);
+void print_rstring(const char *const format,
+		int i, va_list ap, int *nb_printed);
 int _printf(const char *const format, ...);
 void print_int(const char *const format, int i, va_list ap, int *nb_printed);
 
-void print_unsignedInt(const char *const format, int i, va_list ap, int *nb_printed);
+void print_unsignedInt(const char *const format,
+		int i, va_list ap, int *nb_printed);
 void print_octal(const char *const format, int i, va_list ap, int *nb_printed);
-void print_Upper_Hexa(const char *const format, int i, va_list ap, int *nb_printed);
-void print_Lower_Hexa(const char *const format, int i, va_list ap, int *nb_printed);
+void print_Upper_Hexa(const char *const format,
+		int i, va_list ap, int *nb_printed);
+void print_Lower_Hexa(const char *const format,
+		int i, va_list ap, int *nb_printed);
 
 void print_b(const char *const format, int i, va_list ap, int *nb_printed);
 void print_S(const char *const format, int i, va_list ap, int *nb_printed);
